0e45bf84cceefc07c585a6a496972504db755c3c
Motheo-mto <motsemmemotheo@gmail.com>
2026-07-23 19:10:43 +0200

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        HashMap<Character, Integer> charCount = new HashMap<>();
+
+        for(int i=0;i<s.length();i++)
+        {
+            char ch = s.charAt(i);
+            charCount.put(ch,charCount.getOrDefault(ch,0)+1);
+        }
+
+        for(int i=0;i<t.length();i++)
+        {
+            char ch = t.charAt(i);
+
+            if(!charCount.containsKey(ch)) return false;
+
+            charCount.put(ch,charCount.get(ch)-1);
+
+            if(charCount.get(ch)<0) return false;
+        }
+        return true;
+    }
+}
